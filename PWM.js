82a931777ab096c8f36fc9a05a4bc001cb11ad6f
vincefeil <54681226+vincefeil@users.noreply.github.com>
2019-11-14 17:52:53 -0700

    Added comments

--- PWM.js
+++ PWM.js
@@ -1,26 +1,27 @@
 //Varying a PWM signal
 basic.forever(function () {
-    for (let i = 9; i < 171; i += 9) {
+    for (let i = 9; i < 171; i += 9) {//using the for loop to increase the value of the pulse with in 5% increments starting at 5% of 180
+                                        
 
-        basic.pause(100)
-        /*if (i > 170) {
+        basic.pause(100)//pause so the signal change can be seen on the scope
+        /*if (i > 170) {//used for trouble shooting
             i = 9
         }*/
 
 
 
-        pins.servoWritePin(AnalogPin.P0, i)
+        pins.servoWritePin(AnalogPin.P0, i)//write the value of i to P0
     }
-    for (let i = 171; i > 9; i += -9) {
+    for (let i = 171; i > 9; i += -9) {//using the for loop to decrease the value of the pulse with in 5% increments starting at 95% of 180
 
-        basic.pause(100)
-        /*if (i > 170) {
+        basic.pause(100)//pause so the signal change can be seen on the scope
+        /*if (i > 170) {//used for trouble shooting
             i = 9
         }*/
 
 
 
-        pins.servoWritePin(AnalogPin.P0, i)
+        pins.servoWritePin(AnalogPin.P0, i)//write the value of i to P0
     }
 
 })

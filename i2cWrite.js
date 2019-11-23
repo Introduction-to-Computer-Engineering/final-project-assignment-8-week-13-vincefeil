9658e30d8ad27417c6eeb463c80c2754e7690de4
vincefeil <54681226+vincefeil@users.noreply.github.com>
2019-11-23 15:16:29 -0700

    Modified to change values while running

--- i2cWrite.js
+++ i2cWrite.js
@@ -1,8 +1,8 @@
 basic.forever(function () {
-
-    pins.i2cWriteNumber(0x1E, 1, NumberFormat.UInt8LE)/*writes a value of 1 to address 0x1E. 
-    this is an internal address and will show address and data frames*/
-    basic.pause(100)//pause for 100ms to let the user see the display and the scope to trigger
-
-
+    pins.i2cWriteNumber(0x19, 0, NumberFormat.Int8LE)
+    basic.pause(700)
+    pins.i2cWriteNumber(0x1E, 11, NumberFormat.Int8LE)
+    basic.pause(700)
 })
+
+
